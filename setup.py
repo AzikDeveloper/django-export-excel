2a2885370da7eba18a7038bfaa9fa61943591d06
Azizbek Xushnazarov <a.khushnazarov@uic.group>
2025-01-07 15:37:27 +0500

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_export_excel',
+    version='1.0.0',
+    description='Django package for exporting data to Excel file with included admin integration',
+    author='Azizbek Xushnazarov',
+    author_email='azikdevapps@gmail.com',
+    url='https://github.com/AzikDeveloper/django-export-excel',
+    packages=find_packages(),
+)
