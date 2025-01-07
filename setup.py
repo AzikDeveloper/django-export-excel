98d605a5364f5afb508857ac666205377caa8b7f
Azizbek Xushnazarov <a.khushnazarov@uic.group>
2025-01-07 17:05:43 +0500

    ref

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='django_export_excel',
-    version='1.0.4',
+    version='1.0.5',
     description='Django package for exporting data to Excel file with included admin integration',
     author='Azizbek Xushnazarov',
     author_email='azikdevapps@gmail.com',
